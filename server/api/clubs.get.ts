e67eb07a254767ae151aa7ea878e73584709b1b1
Johannes Abendroth <johannes.abendroth@gmail.com>
2026-06-03 19:42:09 +0200

    Revert "feat: add isHidden for club"
    This reverts commit 51d8c731d4a5c216fe41ea3a338592146bd8b2e8.

--- server/api/clubs.get.ts
+++ server/api/clubs.get.ts
@@ -2,7 +2,7 @@ import { prisma } from '~/server/utils/prisma'
 
 export default defineEventHandler(async () => {
   const clubs = await prisma.club.findMany({
-    where: { isSetupDone: true, isHidden: false },
+    where: { isSetupDone: true },
     select: { name: true, slug: true },
     orderBy: { name: 'asc' },
   })
