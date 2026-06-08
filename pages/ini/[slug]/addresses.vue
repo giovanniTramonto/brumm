1b207c773e18bc3c2211dd03d2bb8512c7acb5c1
Johannes Abendroth <johannes.abendroth@gmail.com>
2026-06-08 22:22:38 +0200

    fix: address tables

--- pages/ini/[slug]/addresses.vue
+++ pages/ini/[slug]/addresses.vue
@@ -62,82 +62,14 @@ const groupedSections = computed(() => {
     <LoadingBrumm v-if="isLoading" />
 
     <template v-else-if="groupedSections && groupedSections.length > 0">
-      <div v-for="section in groupedSections" :key="section.name" class="card overflow-x-auto mb-6">
+      <div v-for="section in groupedSections" :key="section.name" class="card mb-6">
         <h2 class="mb-4 text-xl font-semibold text-gray-900">{{ section.name }}</h2>
-        <table class="min-w-full text-sm">
-          <thead>
-            <tr class="border-b border-gray-200 text-left text-xs font-medium uppercase tracking-wide text-gray-500">
-              <th class="pb-3 pr-8 w-1/5">Kind</th>
-              <th class="pb-3 pr-8">Geboren</th>
-              <th class="pb-3 pr-8 w-1/5">Adresse</th>
-              <th class="pb-3 pr-8">Name</th>
-              <th class="pb-3 pr-8">E-Mail</th>
-              <th class="pb-3">Telefon</th>
-            </tr>
-          </thead>
-          <tbody>
-            <template v-for="m in section.members" :key="`${m.firstName}-${m.lastName}`">
-              <tr class="border-t border-gray-100">
-                <td :rowspan="m.guardian2Name || m.email2 ? 2 : 1" class="py-4 pr-8 w-1/5 font-medium text-gray-900 whitespace-nowrap align-middle">
-                  {{ m.firstName }} {{ m.lastName }}
-                </td>
-                <td :rowspan="m.guardian2Name || m.email2 ? 2 : 1" class="py-4 pr-8 text-gray-600 align-middle whitespace-nowrap">
-                  {{ new Date(m.birthDate).toLocaleDateString('de-DE') }}
-                </td>
-                <td :rowspan="m.guardian2Name || m.email2 ? 2 : 1" class="py-4 pr-8 w-1/5 text-gray-600 align-middle">
-                  {{ m.address ?? '–' }}
-                </td>
-                <td class="pt-4 pb-1 pr-8 text-gray-700">{{ m.guardian1Name ?? '' }}</td>
-                <td class="pt-4 pb-1 pr-8 text-gray-700"><a :href="`mailto:${m.email1}`" class="text-blue-600 hover:underline">{{ m.email1 }}</a></td>
-                <td class="pt-4 pb-1 text-gray-700"><a v-if="m.phone1" :href="`tel:${m.phone1}`" class="text-blue-600 hover:underline">{{ m.phone1 }}</a></td>
-              </tr>
-              <tr v-if="m.guardian2Name || m.email2">
-                <td class="pb-4 pr-8 text-gray-700">{{ m.guardian2Name ?? '' }}</td>
-                <td class="pb-4 pr-8 text-gray-700"><a v-if="m.email2" :href="`mailto:${m.email2}`" class="text-blue-600 hover:underline">{{ m.email2 }}</a></td>
-                <td class="pb-4 text-gray-700"><a v-if="m.phone2" :href="`tel:${m.phone2}`" class="text-blue-600 hover:underline">{{ m.phone2 }}</a></td>
-              </tr>
-            </template>
-          </tbody>
-        </table>
+        <AddressTable :members="section.members" />
       </div>
     </template>
 
-    <div v-else class="card overflow-x-auto">
-      <table class="min-w-full text-sm">
-        <thead>
-          <tr class="border-b border-gray-200 text-left text-xs font-medium uppercase tracking-wide text-gray-500">
-            <th class="pb-3 pr-8 w-1/5">Kind</th>
-            <th class="pb-3 pr-8">Geboren</th>
-            <th class="pb-3 pr-8 w-1/5">Adresse</th>
-            <th class="pb-3 pr-8">Name</th>
-            <th class="pb-3 pr-8">E-Mail</th>
-            <th class="pb-3">Telefon</th>
-          </tr>
-        </thead>
-        <tbody>
-          <template v-for="m in addresses" :key="`${m.firstName}-${m.lastName}`">
-            <tr class="border-t border-gray-100">
-              <td :rowspan="m.guardian2Name || m.email2 ? 2 : 1" class="py-4 pr-8 w-1/5 font-medium text-gray-900 whitespace-nowrap align-middle">
-                {{ m.firstName }} {{ m.lastName }}
-              </td>
-              <td :rowspan="m.guardian2Name || m.email2 ? 2 : 1" class="py-4 pr-8 text-gray-600 align-middle whitespace-nowrap">
-                {{ new Date(m.birthDate).toLocaleDateString('de-DE') }}
-              </td>
-              <td :rowspan="m.guardian2Name || m.email2 ? 2 : 1" class="py-4 pr-8 w-1/5 text-gray-600 align-middle">
-                {{ m.address ?? '–' }}
-              </td>
-              <td class="pt-4 pb-1 pr-8 text-gray-700">{{ m.guardian1Name ?? '' }}</td>
-              <td class="pt-4 pb-1 pr-8 text-gray-700">{{ m.email1 }}</td>
-              <td class="pt-4 pb-1 text-gray-700">{{ m.phone1 ?? '' }}</td>
-            </tr>
-            <tr v-if="m.guardian2Name || m.email2">
-              <td class="pb-4 pr-8 text-gray-700">{{ m.guardian2Name ?? '' }}</td>
-              <td class="pb-4 pr-8 text-gray-700">{{ m.email2 ?? '' }}</td>
-              <td class="pb-4 text-gray-700">{{ m.phone2 ?? '' }}</td>
-            </tr>
-          </template>
-        </tbody>
-      </table>
+    <div v-else class="card">
+      <AddressTable :members="addresses" />
       <p v-if="addresses.length === 0" class="mt-4 text-sm text-gray-500">
         Keine aktiven Kinder vorhanden.
       </p>
